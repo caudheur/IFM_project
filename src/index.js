a38520056fddab911db9887883c1309bdeb6d4a4
Paul Kendirgi <p.kendirgi@gmail.com>
2022-09-18 19:01:52 +0000

    Update index.html and index.js

--- src/index.js
+++ src/index.js
@@ -2,34 +2,37 @@ let willingnessToTryNewClothes = 0;
 let diviser = 0;
 let factors = [];
 
-// Factors that influence our decision:
-// name, value, multiplier (how important it is in your decision making)
 
-addFactor("happiness", 80, 3);
-addFactor("self esteem", 80, 1);
-addFactor("need to impress", 90, 2);
-addFactor("metro", 20, -2);
-addFactor("stress", 30, -1);
+
+// addFactor("Mood", 60, 3);
+// addFactor("Weather", 15, 0.5);
+// addFactor("Metro", 50, -2);
+
+// addFactor("Self esteem", 80, 1);
+// addFactor("Need to impress", 90, 2);
+// addFactor("Stress", 30, -1);
+
+
 
 for (let factor of factors) {
-  willingnessToTryNewClothes += factor.amount * factor.importance;
+  willingnessToTryNewClothes += factor.value * factor.coef;
 
-  if (factor.importance > 0) diviser += factor.importance;
+  if (factor.coef > 0) diviser += factor.coef;
 }
 
 function addFactor(name, amount, importance) {
   factors.push({
     name: name,
-    amount: amount,
-    importance: importance
+    value: amount,
+    coef: importance
   });
 }
 
 willingnessToTryNewClothes /= diviser;
-// if( willingnessToTryNewClothes > 100 ) willingnessToTryNewClothes = 100;
-// if( willingnessToTryNewClothes < 0 ) willingnessToTryNewClothes = 0;
+if( isNaN( willingnessToTryNewClothes ) ) willingnessToTryNewClothes = 0;
+if( willingnessToTryNewClothes < 0 ) willingnessToTryNewClothes = 0;
 
-let tableHeaders = ["name", "amount", "importance"];
+let tableHeaders = ["name", "value", "coef"];
 let HTMLTable = "<table><tr>";
 
 for (let header of tableHeaders) {
